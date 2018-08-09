f5435c9ad558118219331c060677b8b14a0f6650
stanleywariat <41842030+stanleywariat@users.noreply.github.com>
2018-08-10 00:09:27 +0200

    Initial commit

---
+++ src/data/Book.java
@@ -0,0 +1,49 @@
+package data;
+
+public class Book extends Publication {
+    // Pola
+    private String author;
+    private int pages;
+    private String isbn;
+
+    // settery i gettery
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Konstruktory
+    public Book(String title, String author, int year, int pages, String publisher,
+                String isbn) {
+        super(year, title, publisher);
+        this.setAuthor(author);
+        this.setPages(pages);
+        this.setIsbn(isbn);
+    }
+
+
+    public void printInfo() {
+        String info = getTitle() + "; " + getAuthor() + "; " + getYear() + "; " + getPages()
+                + "; " + getPublisher() + "; " + getIsbn();
+        System.out.println(info);
+    }
+}
