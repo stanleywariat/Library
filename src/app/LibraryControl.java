5bd2cf7f441fd313c037a3ba999c69cdd94de27d
stanleywariat <41842030+stanleywariat@users.noreply.github.com>
2018-08-25 15:10:29 +0200

    enum Option moved to LibraryControl.java

--- src/app/LibraryControl.java
+++ src/app/LibraryControl.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 
 
+
 import utils.DataReader;
 import utils.LibraryUtils;
 import data.Book;
@@ -80,5 +81,38 @@ public class LibraryControl {
 
     private void printMagazines() {
         LibraryUtils.printMagazines(library);
+    }
+
+    //**class
+    private enum Option {
+        EXIT(0, "Wyjście z programu"),
+        ADD_BOOK(1, "Dodanie książki"),
+        ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
+        PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
+        PRINT_MAGAZINES(4, "Wyświetlenie dostępnych magazynów/gazet");
+
+        private int value;
+        private String description;
+
+        Option(int value, String desc) {
+            this.value = value;
+            this.description = desc;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        public static Option createFromInt(int option) throws NoSuchElementException {
+            Option result = null;
+            try {
+                result = Option.values()[option];
+            } catch(ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchElementException("Brak elementu o wskazanym ID");
+            }
+
+            return result;
+        }
     }
 }
